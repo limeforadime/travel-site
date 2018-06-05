be09d782b2c49a6cc000d6deee3be2876bc9bef8
David <david_gilbert64@yahoo.com>
2018-06-05 16:03:40 -0700

    Finished sticky-header

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class StickyHeader {
@@ -6,10 +7,24 @@ class StickyHeader {
     constructor() {
         this.siteHeader = $(".site-header");
         this.headerTriggerElement = $(".large-hero__title");
+        // want a variable that represents a collection of our page section elements.
+        // They all have page-section in common, so target them based on that.
+        this.pageSections = $(".page-section");
+        this.headerLinks = $(".primary-nav a");
         this.createHeaderWaypoint();
+        this.createPageSectionWaypoints();
+        this.addSmoothScrolling();
     }
 
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll( {
+            speed: "auto",
+            autoCoefficient: 2
+        });
+    }
     createHeaderWaypoint() {
+        // reference variable to point to actual class object since it cannot be 
+        // reached once inside nested functions.
         var that = this;
         new Waypoint({
             element: this.headerTriggerElement[0],
@@ -21,6 +36,39 @@ class StickyHeader {
                     that.siteHeader.removeClass("site-header--dark");
                 }
             }
+        });
+    }
+
+    createPageSectionWaypoints() {
+        var that = this;
+        this.pageSections.each( function() {
+            var currentPageSection = this;
+
+            //create waypoints in the DOWN direction
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if (direction == "down") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }
+                },
+                offset: "18%"
+            });
+
+            //create waypoints in the UP direction
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if (direction == "up") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }
+                },
+                offset: "-40%"
+            });
         });
     }
 }
